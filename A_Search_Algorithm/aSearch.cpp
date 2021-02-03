29abff4a3750a33883d99fcac5353f0b7f6e1f98
JobCollins <dulo.collins@gmail.com>
2021-02-03 11:01:46 +0300

    Chore: Started the code implementation
    started the Search function tha will implement A*search algorithm.
    CellString and PrintBoard are borrowed from gridVector.cpp located outside this folder

---
+++ A_Search_Algorithm/aSearch.cpp
@@ -0,0 +1,80 @@
+#include<iostream>
+#include<vector>
+#include<sstream>
+#include<fstream>
+
+
+using namespace std;
+
+
+vector<vector<State>> Search(vector<vector<int>> grid, int start[2], int goal[2]){
+    
+    vector<vector<State>> board;
+    
+    cout<<"No path found!";
+
+    return board;
+}
+
+//taken from ../gridVector.cpp
+enum State {kEmpty, kObstacle} state;
+
+void CellString(State state){
+    switch (state)
+    {
+    case kEmpty:
+        /* code */
+        cout<<"0 ";
+        break;
+    case kObstacle:
+        /* code */
+        cout<<"⛰️ ";
+        break;
+    default:
+        break;
+    }
+}
+
+void PrintBoard(vector<vector<int>> vec){
+    for (auto i = 0; i < vec.size(); i++)
+    {
+        /* code */
+        for (auto j = 0; j < vec[i].size(); j++)
+        {
+            /* code */
+
+            // cout<<vec[i][j]<<' ';
+            if (vec[i][j]==0)
+            {
+                /* code */
+                CellString(kEmpty);
+            }
+            else if(vec[i][j]==1)
+            {
+                /* code */
+                CellString(kObstacle);
+            }
+            else
+            {
+                break;
+            }
+            
+        }
+        cout<<endl;
+        
+    }
+    
+}
+
+int main()
+{   
+    int start[2] = {0,0};
+    int goal[2] = {4,5};
+
+
+    int solution = Search(board, start, goal);
+
+    PrintBoard(solution);
+
+    return 0;
+}
